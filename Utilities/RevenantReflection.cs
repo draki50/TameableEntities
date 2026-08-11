78d95a904325361819a6e3afe2abaf15425d7919
draki50 <pyro_home@hotmail.com>
2026-08-12 01:11:01 +0200

    Delete Utilities directory

--- Utilities/RevenantReflection.cs
+++
@@ -1,8 +0,0 @@
-using HarmonyLib;
-using RimWorld;
-
-public static class RevenantReflection
-{
-    public static readonly AccessTools.FieldRef<CompRevenant, int> BecomeInvisibleTick =
-        AccessTools.FieldRefAccess<CompRevenant, int>("becomeInvisibleTick");
-}
